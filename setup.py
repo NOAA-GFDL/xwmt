13343b8115607853ef0daa5a043516e01aa6723c
Jan-Erik Tesdal <je.tesdal@googlemail.com>
2021-09-16 08:12:58 -0400

    populating package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='xwmt',
+    url='https://github.com/jetesdal/xwmt.git',
+    version='0.1dev',
+    packages=find_packages(),
+    license='MIT',
+)
---
+++ xwmt/__init__.py
@@ -0,0 +1,1 @@
+from .swmt import swmt
