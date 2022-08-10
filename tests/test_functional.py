72bdb839c68383b37b2aa452ea1d63720fb5d85c
gmacgilchrist <graemem@princeton.edu>
2022-08-10 14:42:46 -0400

    adds test for ungrouped tendencies for sigma0

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -5,31 +5,40 @@ import xwmt
 
 ds = xr.open_dataset("xwmt_test_data.nc", use_cftime=True)
 ds = ds.rename({"xh": "x", "yh": "y", "geolat": "lat", "geolon": "lon"})
-    
-def test_functional_default_sigma0():
+
+## Default settings
+# sigma
+def test_functional_sigma0_default():
     total = xwmt.swmt(ds).G("sigma0")
     assert np.allclose(total.sum(), -2.1251855e09)
     
-def test_functional_default_sigma1():
+def test_functional_sigma1_default():
     total = xwmt.swmt(ds).G("sigma1")
     assert np.allclose(total.sum(), -1.8154245e09)
 
-def test_functional_default_sigma2():
+def test_functional_sigma2_default():
     total = xwmt.swmt(ds).G("sigma2")
     assert np.allclose(total.sum(), -1.5430248e09)
     
-def test_functional_default_sigma3():
+def test_functional_sigma3_default():
     total = xwmt.swmt(ds).G("sigma3")
     assert np.allclose(total.sum(), -1.32300659e09)
     
-def test_functional_default_sigma4():
+def test_functional_sigma4_default():
     total = xwmt.swmt(ds).G("sigma4")
     assert np.allclose(total.sum(), -1.20211688e09)
 
-def test_functional_default_theta():
+# heat
+def test_functional_theta_default():
     total = xwmt.swmt(ds).G("theta")
     assert np.allclose(total.sum(), -1.65192249e09)
-
-def test_functional_default_salt():
+# salt
+def test_functional_salt_default():
     total = xwmt.swmt(ds).G("salt")
     assert np.allclose(total.sum(), -3.01411116e08)
+
+## Tendencies not grouped
+def test_functional_sigma0_notgrouped():
+    total = xwmt.swmt(ds).G("sigma0",group_tend=False)
+    assert (np.allclose(total["heat"].sum(), -2.8727879e09)) & (np.allclose(total["heat"].sum(), 7.47602401e08))
+    
