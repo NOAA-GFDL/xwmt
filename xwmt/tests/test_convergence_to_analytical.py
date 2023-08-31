d5528f7a3ffd69dd7bb804571938d5665f26ed90
Henri Drake <henrifdrake@gmail.com>
2023-08-31 14:57:32 -0400

    Fixed both analytical and MOM6 example data tests
    - Introduced `conftest.py` file to download the MOM6 Baltic test dataset
    (if it does not already exist in the local `/xwmt/tests/` path) and
    hold various helper methods/functions for the analytical example
    
    - Removed download from the ci since it should automatically do this whenever
    testing the code with `pytest`, whether locally in developer tests or via
    github's CI workflow.
    
    - Now that 2D and 3D transformations are merged into a single process,
    these are tested together using the new `test_integrate_transformations.py`.

--- xwmt/tests/test_convergence_to_analytical.py
+++ xwmt/tests/test_convergence_to_analytical.py
@@ -1,187 +1,37 @@
 import pytest
 import numpy as np
-import xarray as xr
-import xwmt
-import xgcm
 
-def test_analytical_errors(rtol=1.e-5):
+def test_analytical_errors(helpers):
     passing = []
-    for (lam, tend, loc, lay) in zip(exps['lam_profile'], exps['extensive_tendency'], exps['local_wmt'], exps['layer_wmt']):
+    for (lam, tend, loc, lay) in zip(
+            helpers.exps['lam_profile'],
+            helpers.exps['extensive_tendency'],
+            helpers.exps['local_wmt'],
+            helpers.exps['layer_wmt']
+        ):
         # pick discretizations with no shared multiples so levels do not coincide
-        wmt = idealized_transformations(tend, lam, Nz=5**9, Nlam=2**10)
-        err = mean_absolute_relative_errors(wmt, loc, lay)[0]
-        passing.append(err < rtol)
+        wmt = helpers.idealized_transformations(tend, lam, Nz=5**9, Nlam=2**10)
+        err = helpers.mean_absolute_relative_errors(wmt, loc, lay)[0]
+        passing.append(err < 1.e-5)
     assert np.all(passing)
 
-def test_analytical_error_convergence():
+def test_analytical_error_convergence(helpers):
     passing = []
-    for (lam, tend, loc, lay) in zip(exps['lam_profile'], exps['extensive_tendency'], exps['local_wmt'], exps['layer_wmt']):
-        wmt_lo =     idealized_transformations(tend, lam, Nz=5**6, Nlam=2**4)
-        wmt_hi_z =   idealized_transformations(tend, lam, Nz=5**9, Nlam=2**4)
-        wmt_hi_lam = idealized_transformations(tend, lam, Nz=5**6, Nlam=2**8)
-        err_lo =     mean_absolute_relative_errors(wmt_lo,     loc, lay)
-        err_hi_z =   mean_absolute_relative_errors(wmt_hi_z,   loc, lay)
-        err_hi_lam = mean_absolute_relative_errors(wmt_hi_lam, loc, lay)
+    for (lam, tend, loc, lay) in zip(
+            helpers.exps['lam_profile'],
+            helpers.exps['extensive_tendency'],
+            helpers.exps['local_wmt'],
+            helpers.exps['layer_wmt']
+        ):
+        wmt_lo =     helpers.idealized_transformations(tend, lam, Nz=5**6, Nlam=2**4)
+        wmt_hi_z =   helpers.idealized_transformations(tend, lam, Nz=5**9, Nlam=2**4)
+        wmt_hi_lam = helpers.idealized_transformations(tend, lam, Nz=5**6, Nlam=2**8)
+        err_lo =     helpers.mean_absolute_relative_errors(wmt_lo,     loc, lay)
+        err_hi_z =   helpers.mean_absolute_relative_errors(wmt_hi_z,   loc, lay)
+        err_hi_lam = helpers.mean_absolute_relative_errors(wmt_hi_lam, loc, lay)
         passing.append(
             (err_hi_z[0]   <  err_lo[0]) and
             (err_hi_z[1]   <  err_lo[1]) and
             (err_hi_lam[1] <  err_lo[1])
         )
     assert np.all(passing)
-
-def idealized_transformations(extensive_tendency, lam_profile, Nz=1e3, Nlam=8):
-    bins = np.linspace(0., 1., int(Nlam)+1)
-    
-    ds = xr.Dataset()
-    ds = ds.assign_coords({
-        'z_i': xr.DataArray(np.linspace(0., 1., int(Nz)+1), dims=("z_i",)),
-        'z_l': xr.DataArray(np.linspace(1. /Nz, 1. - 1. /Nz, int(Nz)), dims=("z_l",)),
-    })
-    ds = ds.assign_coords({'dz': xr.DataArray(np.diff(ds.z_i.values), dims=("z_l",))})
-    ds['tendency_name'] = xr.DataArray(extensive_tendency(ds.z_i.values), coords=(ds.z_l,))*ds.dz
-    ds['temperature'] = xr.DataArray(lam_profile(ds.z_l.values), coords=(ds.z_l,))
-
-    # expand to horizontal dimension and add grid metrics
-    ds = ds.expand_dims(dim=('x', 'y')).assign_coords({'x':xr.DataArray([1.], dims=('x',)), 'y':xr.DataArray([1.], dims=('y',))})
-    ds = ds.assign_coords({'rA': xr.DataArray([[1.]], dims=('x','y',))})
-
-    metrics = {
-        ('X', 'Y'): ['rA'] # Areas
-    }
-    coords = {
-        'X': {'center': 'x',},
-        'Y': {'center': 'y',},
-        'Z': {'center': 'z_l', 'outer': 'z_i'},
-    }
-    grid = xgcm.Grid(ds, coords=coords, metrics=metrics, periodic=['X', 'Y'])
-    
-    budget_dict = {
-        'heat': {
-            'lambda': 'temperature',
-            'lhs': {'tendency':'tendency_name'},
-            'rhs': {}
-        },
-        'salt': {
-            'lambda': None,
-            'rhs': {},
-            'lhs': {}
-        }
-    }
-
-    wmt = xwmt.WaterMassTransformations(ds, grid, budget_dict, t_name='temperature', cp=1., rho_ref=1.)
-    T = wmt.integrate_transformations("temperature", bins=bins, sum_components=False)
-    T = T.assign_coords({'temperature_i': xr.DataArray(bins, dims=("temperature_i",))})
-    return T
-
-def mean_absolute_relative_errors(wmt_xwmt, wmt_local_exact, wmt_layer_exact):
-    def absolute_relative_errors(wmt, wmt_ref):
-        return np.abs((wmt - wmt_ref)/wmt_ref).where(np.abs(wmt_ref)>1.e-5).mean(skipna=True).values
-    return (
-        absolute_relative_errors(
-            wmt_xwmt.tendency_name,
-            wmt_layer_exact(wmt_xwmt.temperature_i.values)
-        ),
-        absolute_relative_errors(
-            wmt_xwmt.tendency_name,
-            wmt_local_exact(wmt_xwmt.temperature.values)
-        )
-    )
-
-# Extensive (layer-integrated) analytical tendency profiles
-def diffusive_extensive_tendency(z_i):
-    def f(z):
-        return -np.cos(2*np.pi*z)/(2*np.pi)
-    return np.diff(f(z_i))/np.diff(z_i)
-
-def constant_extensive_tendency(z_i):
-    return np.diff(z_i)/np.diff(z_i)
-
-def constant_plus_diffusion_tendency(z_i):
-    return diffusive_extensive_tendency(z_i) + constant_extensive_tendency(z_i)
-
-def differential_heating_layer(z_i):
-    sign = 2*np.float64(z_i>0.5)-1
-    out = np.diff(sign*z_i)/np.diff(z_i)
-    out[(z_i[:-1]<=0.5)&(0.5<=z_i[1:])] = 0.
-    return out
-
-# Stratification profiles
-def lam_const_dlamdz(z):
-    return z
-
-def lam_linear_dlamdz(z):
-    return z**2
-
-def lam_overturning_dlamdz(z):
-    return 1 - (2*z - 1)**2
-
-def lam_vanishing_dlamdz(z):
-    sign = 2*np.float64(z>0.5)-1
-    return (1 + sign*(2*z - 1)**2)/2.
-
-# Analytical point-wise water mass transformations
-def constant_plus_diffusion_local_wmt_dlamdz_constant(lam):
-    return np.sin(2*np.pi*lam) + 1.
-
-def constant_plus_diffusion_local_wmt_dlamdz_linear(lam):
-    return (np.sin(2*np.pi*np.sqrt(lam)) + 1.)/(2*np.sqrt(lam))
-
-def constant_plus_diffusion_local_wmt_dlamdz_overturning(lam):
-    return 1/(2*np.sqrt(1-lam))
-
-
-def differential_heating_local_wmt_dlamdz_vanishing(lam):
-    sign = 2*np.float64(lam>0.5)-1
-    return sign/(2*np.sqrt(sign*(2*lam-1)))
-
-# Analytical layer-averaged water mass transformations
-def constant_plus_diffusion_layer_wmt_dlamdz_constant(lam_bins):
-    def f(lam):
-        return -np.cos(2*np.pi*lam)/(2*np.pi) + lam
-    return np.diff(f(lam_bins))/np.diff(lam_bins)
-
-def constant_plus_diffusion_layer_wmt_dlamdz_linear(lam_bins):
-    def f(lam):
-        return -np.cos(2*np.pi*np.sqrt(lam))/(2*np.pi) + np.sqrt(lam)
-    return np.diff(f(lam_bins))/np.diff(lam_bins)
-
-def constant_plus_diffusion_layer_wmt_dlamdz_overturning(lam_bins):
-    def f(lam):
-        return 1 - np.sqrt(1 - lam)
-    return np.diff(f(lam_bins))/np.diff(lam_bins)
-
-def differential_heating_layer_wmt_dlamdz_vanishing(lam_bins):
-    def f(lam):
-        sign = 2*np.float64(lam>0.5)-1
-        u = 1 + sign*np.sqrt(sign*(2*lam-1))
-        return sign*0.5*u
-    out = np.diff(f(lam_bins))/np.diff(lam_bins)
-    out[(lam_bins[:-1]<=0.5)&(0.5<=lam_bins[1:])] = np.nan
-    return out
-
-exps = {
-    "extensive_tendency": [
-        constant_plus_diffusion_tendency,
-        constant_plus_diffusion_tendency,
-        constant_plus_diffusion_tendency,
-        differential_heating_layer
-    ],
-    "lam_profile": [
-        lam_const_dlamdz,
-        lam_linear_dlamdz,
-        lam_overturning_dlamdz,
-        lam_vanishing_dlamdz
-    ],
-    "local_wmt": [
-        constant_plus_diffusion_local_wmt_dlamdz_constant,
-        constant_plus_diffusion_local_wmt_dlamdz_linear,
-        constant_plus_diffusion_local_wmt_dlamdz_overturning,
-        differential_heating_local_wmt_dlamdz_vanishing
-    ],
-    "layer_wmt":[
-        constant_plus_diffusion_layer_wmt_dlamdz_constant,
-        constant_plus_diffusion_layer_wmt_dlamdz_linear,
-        constant_plus_diffusion_layer_wmt_dlamdz_overturning,
-        differential_heating_layer_wmt_dlamdz_vanishing
-    ]
-}
---
+++ xwmt/tests/test_integrate_transformations.py
@@ -0,0 +1,81 @@
+import pytest
+import numpy as np
+import xarray as xr
+import xbudget
+import xgcm
+import xwmt
+
+ds = xr.open_dataset("xwmb_test_data_Baltic_3d.20230830.nc", use_cftime=True).isel(time=0)
+budgets_dict = xbudget.load_preset_budget(model="MOM6")
+xbudget.collect_budgets(ds, budgets_dict)
+simple_budgets = xbudget.aggregate(budgets_dict)
+coords = {
+    'X': {'center': 'xh', 'outer': 'xq'},
+    'Y': {'center': 'yh', 'outer': 'yq'},
+    'Z': {'center': 'zl', 'outer': 'zi'},
+}
+metrics = {
+    ('X','Y'): "areacello",
+}
+grid = xgcm.Grid(ds, coords=coords, metrics=metrics, periodic=None, autoparse_metadata=False)
+wmt = xwmt.WaterMassTransformations(grid, simple_budgets, method="xgcm")
+
+## Default parameters except: wide bin range to cover all cases and group processes
+kwargs = {'bins': np.arange(-10, 100, 1.), 'group_processes': True}
+
+# heat
+def test_functional_3d_theta_default():
+    total_wmt = wmt.integrate_transformations("heat", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        -7156040943.980093,
+    )
+
+
+# salt
+def test_functional_3d_salt_default():
+    total_wmt = wmt.integrate_transformations("salt", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        78005966.15053421,
+    )
+
+# sigma
+def test_functional_3d_sigma0_default():
+    total_wmt = wmt.integrate_transformations("sigma0", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        550052085.4891472,
+    )
+
+
+def test_functional_3d_sigma1_default():
+    total_wmt = wmt.integrate_transformations("sigma1", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        550052085.4891474,
+    )
+
+
+def test_functional_3d_sigma2_default():
+    total_wmt = wmt.integrate_transformations("sigma2", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        550065820.7692934,
+    )
+
+
+def test_functional_3d_sigma3_default():
+    total_wmt = wmt.integrate_transformations("sigma3", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        550052085.4891478,
+    )
+
+
+def test_functional_3d_sigma4_default():
+    total_wmt = wmt.integrate_transformations("sigma4", **kwargs)['process_material_derivative']
+    assert np.isclose(
+        total_wmt.sum().values,
+        550052085.489147,
+    )
