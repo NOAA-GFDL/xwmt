4d4a44a866bddcb4ce2e8437a3fffb04af69a351
Henri Drake <henrifdrake@gmail.com>
2023-10-19 17:56:18 -0400

    Update example notebooks

--- xwmt/__init__.py
+++ xwmt/__init__.py
@@ -1,5 +1,5 @@
 """ xwmt: xarray-friendly water mass transformation """
 
-from .wm import WaterMass
+from .wm import WaterMass, add_gridcoords
 from .wmt import WaterMassTransformations
 from .version import __version__
