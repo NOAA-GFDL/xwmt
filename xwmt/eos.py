34e723a6531a65ac0b8e03577dfd5344198e0a92
Henri Drake <hfdrake@uci.edu>
2026-03-31 08:01:39 -0700

    Added EOS-80 via python-seawater package
    See https://pypi.org/project/seawater/
    
    While EOS-80 is considered obsolete, many ocean models still use it.
    Thus, it is still useful for self-consistency while analyzing
    output from ocean models.
    
    Related `momlevel` issue: https://github.com/jkrasting/momlevel/issues/57
    Related `xwmt` issue: https://github.com/NOAA-GFDL/xwmt/issues/55

--- xwmt/eos.py
+++ xwmt/eos.py
@@ -1,9 +1,10 @@
+import seawater as sw
 import fastjmd95
 import momlevel
 import gsw
 import xarray as xr
 
-eos_list = ["TEOS-10", "JMD-95", "Wright-97"]
+eos_list = ["TEOS-10", "EOS-80", "JMD-95", "Wright-97"]
 def _raise_eos_error():
     raise ValueError(f"This eos is not currently supported; choose from f{eos_list}.")
 
@@ -12,6 +13,10 @@ def rho(t, s, p, eos):
         return xr.apply_ufunc(
                 gsw.rho, s, t, p, dask="parallelized"
             )
+    elif eos=="EOS-80":
+        return xr.apply_ufunc(
+                sw.dens, s, t, p, dask="parallelized"
+        )
     elif eos=="JMD-95":
         return fastjmd95.rho(s, t, p)
     elif eos=="Wright-97":
@@ -24,6 +29,10 @@ def alpha(t, s, p, eos):
         return xr.apply_ufunc(
                 gsw.alpha, s, t, p, dask="parallelized"
             )
+    elif eos=="EOS-80":
+        return xr.apply_ufunc(
+                sw.alpha, s, t, p, dask="parallelized"
+        )
     elif eos=="JMD-95":
         return -fastjmd95.drhodt(s, t, p)/rho(t, s, p, eos="JMD-95")
     elif eos=="Wright-97":
@@ -36,6 +45,10 @@ def beta(t, s, p, eos):
         return xr.apply_ufunc(
                 gsw.beta, s, t, p, dask="parallelized"
             )
+    elif eos=="EOS-80":
+        return xr.apply_ufunc(
+                sw.beta, s, t, p, dask="parallelized"
+        )
     elif eos=="JMD-95":
         return fastjmd95.drhods(s, t, p)/rho(t, s, p, eos="JMD-95")
     elif eos=="Wright-97":
